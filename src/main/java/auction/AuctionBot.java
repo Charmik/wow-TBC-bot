00620bc26bafcb22e05d0d874f5b300ac0a35850
charmik <charmik1994@gmail.com>
2018-03-28 06:02:46 +0300

    update auc-bot

--- src/main/java/auction/AuctionBot.java
+++ src/main/java/auction/AuctionBot.java
@@ -1,11 +1,19 @@
 package auction;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
 
 import farmbot.Bot;
 import org.slf4j.Logger;
@@ -18,32 +26,79 @@ public class AuctionBot {
 
     private static final Logger logger = LoggerFactory.getLogger(Bot.class);
     private static final int MAX_PAGES = 600;
+    private static final int SLEEP1 = 1350;
+    private static final int SLEEP2 = 200;
+    private static final Random random = new Random();
+    private static final String FILE_NAME = "auc_" + SLEEP1 + "_" + SLEEP2 + "_" + random.nextInt(30) + ".txt";
 
     private static final WowInstance wowInstance = new WowInstance("World of Warcraft");
-    private final AuctionManager auctionManager;
+    public final AuctionManager auctionManager;
 
-    public AuctionBot() {
+    private AuctionBot() {
         this.auctionManager = wowInstance.getAuctionManager();
     }
 
-    public static void main(String[] args) throws InterruptedException, FileNotFoundException, UnsupportedEncodingException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         AuctionBot auctionBot = new AuctionBot();
+        logger.info("FILE_NAME=" + FILE_NAME);
         auctionBot.saveDataToFile();
+//        printCurrentPage(auctionBot);
+//        sortAuctionId(auctionBot, "auc_3000_3000_12.txt");
     }
 
-    public void saveDataToFile() throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
+    private static void sortAuctionId(AuctionBot auctionBot, String fileName) throws IOException {
+        List<Item> items = auctionBot.readFromFile(fileName);
+        HashSet<Item> set = new HashSet<>(items);
+        System.out.println(items.size() + " " + set.size());
+        Collections.sort(items);
+
+        List<Integer> ints = items.stream().map(Item::getAuctionId).sorted().collect(Collectors.toList());
+        for (Integer x : ints) {
+            System.out.println(x);
+        }
+    }
+
+    private static void printCurrentPage(AuctionBot auctionBot) {
+        Item[] itemsFromCurrentPage = auctionBot.auctionManager.getItemsFromCurrentPage();
+        for (Item item : itemsFromCurrentPage) {
+            System.out.println(item);
+        }
+    }
+
+    private List<Item> readFromFile(String file) throws IOException {
+        List<Item> list = new ArrayList<>();
+        List<String> strings = Files.readAllLines(Paths.get(file));
+        for (String s : strings) {
+            Object[] arr = Arrays.stream(s.split(" ")).map(Integer::valueOf).toArray();
+            list.add(new Item((Integer) arr[0], (Integer) arr[1], (Integer) arr[2], (Integer) arr[3], (Integer) arr[4], (Integer) arr[5], (Integer) arr[6], (Integer) arr[7]));
+        }
+        return list;
+    }
+
+    private void saveDataToFile() throws FileNotFoundException, UnsupportedEncodingException, InterruptedException {
         List<Item> items = getAllItemsFromAuc();
-        PrintWriter writer = new PrintWriter("auc0.txt", "UTF-8");
+        int oldSize = items.size();
+        //but of the wow, sometimes you see the same items by scanning, so delete the equals by auctionId
+        if (items.size() < 10000) {
+            logger.info("items size only: " + items.size() + " dont save it");
+            return;
+        }
+        items = new ArrayList<>(new HashSet<>(items));
+        String msg = "oldSize:" + oldSize + " setSize:" + items.size() + "  " + ((double) items.size() / (double) oldSize);
+        logger.info(msg);
+
+        PrintWriter writer = new PrintWriter(FILE_NAME, "UTF-8");
+        writer.println(new Date());
+        writer.println(msg);
         for (Item item : items) {
             //logger.info(item.toString());
-            writer.println(item.getAuctionId() + " " + item.getItemId() + " " + item.getCount() + " " + item.getUsableItem() + " " + item.getCurrentBid() + " " + item.getBuyOut() + " "
-                + item.getExpireTime());
+            writer.println(item);
         }
         logger.info("closing file");
         writer.close();
     }
 
-    public List<Item> getAllItemsFromAuc() throws InterruptedException {
+    private List<Item> getAllItemsFromAuc() throws InterruptedException {
         List<Item> items = new ArrayList<>();
         Item[] itemsFromCurrentPage = null;
         List<Integer> readFromMemoryCounts = new ArrayList<>();
@@ -53,25 +108,29 @@ public class AuctionBot {
         int countForceScanning = 0;
         boolean foundLastPage = false;
         for (int page = 1; page <= MAX_PAGES; page++) {
-            logger.info("page=" + page);
+            if (page % 10 == 0) {
+                logger.info("page=" + page);
+            }
             int lastExpiredTime = -1;
             if (itemsFromCurrentPage != null) {
                 lastExpiredTime = itemsFromCurrentPage[0].getExpireTime();
             }
-            int countGetPreviousPage = -1;
+            int countGetPreviousPage = 1;
             long startPageScanning = System.currentTimeMillis();
             boolean flagForceButtonNextPage = false;
             do {
                 itemsFromCurrentPage = auctionManager.getItemsFromCurrentPage();
                 countGetPreviousPage++;
                 //try to force nextPage again, we will miss this page, doesn't matter, bug wow.
-                if (countGetPreviousPage == 5000) {
+                if (countGetPreviousPage % 5000 == 0) {
+                    logger.info("can't read memory too long, try force next page");
                     clickNextPage();
                     flagForceButtonNextPage = true;
                     countForceScanning++;
                 }
                 //found lastPage
-                if (countGetPreviousPage == 10000) {
+                if (countGetPreviousPage == 15000) {
+                    logger.info("can't read memory too long, it should be last page, exit");
                     foundLastPage = true;
                     break;
                 }
@@ -86,17 +145,17 @@ public class AuctionBot {
             }
             //logger.info("countGetPreviousPage=" + countGetPreviousPage);
             readFromMemoryCounts.add(countGetPreviousPage);
-            Thread.sleep(400);
+            Thread.sleep(SLEEP1);
             clickNextPage();
-            Thread.sleep(100);
+            Thread.sleep(SLEEP2);
         }
         logger.info("longestScanning=" + longestScanning);
         logger.info("countForceScanning=" + countForceScanning);
         logger.info(readFromMemoryCounts.toString());
-        logger.info("scanningTime:", (System.currentTimeMillis() - startTime) / 1000);
+        logger.info("scanningTime:" + (System.currentTimeMillis() - startTime) / 1000);
         return items;
-
     }
+
 
     private void clickNextPage() {
         wowInstance.click(WinKey.D4);
--- src/main/java/auction/Item.java
+++ src/main/java/auction/Item.java
@@ -1,14 +1,18 @@
 package auction;
 
-public class Item {
+import java.util.Objects;
+
+public class Item implements Comparable<Item> {
     private final int auctionId;
     private final int itemId;
     private final int count;
     //-1 if you can make "right click to item", 0 otherwise
     private final int usableItem;
+    private final int xxx;
     private final int currentBid;
     private final int buyOut;
     private final int expireTime;
+
 
     //from 3.0.9
     /*
@@ -34,13 +38,33 @@ public class Item {
     unsigned int SaleStatus;        // 0x9C
      */
 
+    public Item(
+        int auctionId,
+        int itemId,
+        int count,
+        int usableItem,
+        int xxx,
+        int currentBid,
+        int buyOut,
+        int expireTime)
+    {
+        this.auctionId = auctionId;
+        this.itemId = itemId;
+        this.count = count;
+        this.usableItem = usableItem;
+        this.xxx = xxx;
+        this.currentBid = currentBid;
+        this.buyOut = buyOut;
+        this.expireTime = expireTime;
+    }
+
     public Item(int[] arr) {
         this.auctionId = arr[1];
         this.itemId = arr[2];
         this.count = arr[23];
         this.usableItem = arr[24];
         // TODO: understand what is it
-        int x = arr[26];
+        this.xxx = arr[26];
         this.currentBid = arr[28];
         this.buyOut = arr[30];
         this.expireTime = arr[31];
@@ -48,15 +72,7 @@ public class Item {
 
     @Override
     public String toString() {
-        return "Item{" +
-            "auctionId=" + auctionId +
-            ", itemId=" + itemId +
-            ", count=" + count +
-            ", usableItem=" + usableItem +
-            ", currentBid=" + currentBid +
-            ", buyOut=" + buyOut +
-            ", expireTime=" + expireTime +
-            '}';
+        return auctionId + " " + itemId + " " + count + " " + usableItem + " " + xxx + " " + currentBid + " " + buyOut + " " + expireTime;
     }
 
     public int getAuctionId() {
@@ -70,6 +86,7 @@ public class Item {
     public int getCount() {
         return count;
     }
+
 
     public int getUsableItem() {
         return usableItem;
@@ -85,5 +102,23 @@ public class Item {
 
     public int getExpireTime() {
         return expireTime;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return auctionId == item.auctionId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(auctionId);
+    }
+
+    @Override
+    public int compareTo(Item o) {
+        return Integer.compare(auctionId, o.auctionId);
     }
 }
