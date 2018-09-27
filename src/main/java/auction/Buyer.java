47033629f820f8289120cd0ae7a18b725e5bb0cb
alexlovkov <charmik1994@gmail.com>
2018-09-27 06:49:53 +0300

    rewrite neutral auction buyer

--- src/main/java/auction/Buyer.java
+++ src/main/java/auction/Buyer.java
@@ -8,10 +8,15 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Optional;
 
 import auction.analyzer.Analyzer;
 import auction.dao.FilesManager;
+import bgbot.Movement;
+import farmbot.Pathing.GlobalGraph;
+import farmbot.Pathing.Graph;
+import javafx.geometry.Point3D;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import util.Utils;
@@ -32,12 +37,14 @@ public class Buyer {
     private static final int MAX_PAGES = 1000;
     private static final int SLEEP1 = 100;
     public static final int SLEEP2 = 1300;
+    private static final int MIN_PAGES = 250;
 
     private static final WowInstance wowInstance = WowInstance.getInstance();
     private final String folder;
     private final FilesManager filesManager;
     private final Analyzer analyzer;
     private final ObjectManager objectManager;
+    private final Movement movement;
     private Player player;
     private CtmManager ctmManager;
     private String tmpFile;
@@ -45,7 +52,7 @@ public class Buyer {
     private boolean scanOnlyFirstPage;
     private boolean firstIteration;
     private AuctionManager auctionManager;
-    private static final int MIN_PAGES = 400;
+    private GlobalGraph graph;
 
     public Buyer(boolean scanOnlyFirstPage, String folder, Analyzer analyzer, FilesManager filesManager) {
         this.folder = folder;
@@ -58,6 +65,15 @@ public class Buyer {
         this.scanOnlyFirstPage = scanOnlyFirstPage;
         // TODO: do we really need it? history is big.
         this.firstIteration = false;
+        if (player.getZone().isStranglethornVale()) {
+            this.graph = new GlobalGraph("routesAuc\\stranglethornVale");
+        } else if (player.getZone().isWinterspring()) {
+            this.graph = new GlobalGraph("routesAuc\\winterspring");
+        }
+        this.movement = new Movement(player, ctmManager, wowInstance, objectManager);
+        if (graph != null) {
+            this.graph.buildGlobalGraph();
+        }
     }
 
     boolean analyze() throws InterruptedException, IOException, ParseException {
@@ -136,7 +152,10 @@ public class Buyer {
         }
 
         if (page < MIN_PAGES) {
-            logger.warn("found not enough pages, something wrong, found only:{}", page);
+            int sleepingTime = 1000 * 60 * 5;
+            logger.warn("found not enough pages, something wrong, found only:{}, sleeping for:{}", page, sleepingTime);
+            Utils.sleep(sleepingTime);
+            resetAuction();
         } else {
             logger.info("found {} pages for the scan", page);
             resetTmpFile();
@@ -158,66 +177,104 @@ public class Buyer {
         int count = -1;
         boolean TEST_FLAG = false;
         logger.info("TEST_FLAG:" + TEST_FLAG);
+        logger.info("calculate auction");
         analyzer.calculate();
+        logger.info("started refreshing auction");
+        boolean firstRun = true;
         for (; ; ) {
             count++;
-            if (count % 1000 == 0) {
+            if (count % 30 == 0) {
                 count = 0;
-                if (wowInstance.getPlayer().isDead()) {
-                    logger.info("played is dead, exit");
-                    return false;
+                boolean isDead = wowInstance.getPlayer().isDead();
+                objectManager.refillPlayers();
+                isDead = isDead | wowInstance.getPlayer().isDead();
+                if (isDead) {
+                    if (!firstRun) {
+                        long sleepBeforeRess = 1000 * 60 * 10;
+                        logger.info("sleeping for:{}", sleepBeforeRess);
+                        Utils.sleep(sleepBeforeRess);
+                    }
+                    firstRun = false;
+                    logger.info("player is dead, trying to ress");
+                    if (player.isDeadLyingDown()) {
+                        logger.info("player isDeadLyingDown");
+                        for (int i = 0; i < 10; i++) {
+                            wowInstance.click(WinKey.D9, 0L);
+                            Utils.sleep(100);
+                        }
+                    }
+                    Utils.sleep(5000);
+                    if (graph != null) {
+                        Point3D point;
+                        // auction points for different locations
+                        if (player.getZone().isStranglethornVale()) {
+                            point = new Point3D(-14417.693, 523.9574, 5.014096);
+                        } else if (player.getZone().isWinterspring()) {
+                            point = new Point3D(6772.7534, -4679.1396, 723.76514);
+                        } else {
+                            // TODO: tanaris
+                            point = null;
+                        }
+                        List<Graph.Vertex> shortestPath = graph.getShortestPath(player.getCoordinates(), point);
+                        boolean success = true;
+                        for (Graph.Vertex vertex : shortestPath) {
+                            if (!movement.goToNextPoint(vertex.coordinates)) {
+                                success = false;
+                                wowInstance.click(WinKey.D0);
+                            }
+                        }
+                        Utils.sleep(1000);
+                        ctmManager.goTo(shortestPath.get(shortestPath.size() - 1).getCoordinates(), false);
+                        for (int i = 0; i < 120; i++) {
+                            wowInstance.click(WinKey.D0, 0L);
+                            Utils.sleep(1000);
+                            if (!player.isDead()) {
+                                break;
+                            }
+                        }
+                        Utils.sleep(2000);
+                        wowInstance.click(WinKey.D0);
+                        Utils.sleep(2000);
+                        // cat form
+                        wowInstance.click(WinKey.D5);
+                        Utils.sleep(2000);
+                        // stealth
+                        wowInstance.click(WinKey.D4);
+                        resetAuction();
+                        //screen shot
+                        wowInstance.click(WinKey.P);
+                        if (!success) {
+                            logger.info("couldn't ress, exit");
+                            ctmManager.stop();
+                            return false;
+                        }
+                    }
                 }
             }
-            /*
-            if (count % 1000 == 0) {
-                try {
-                    analyzer.calculate();
-                } catch (Throwable e) {
-                    //scanFullAuction(true);
-                    logger.error(e.toString());
-                    count = -1;
-                    continue;
-                }
-            }
-            */
+
             Item[] itemsFromCurrentPage = auctionManager.getItemsFromCurrentPage();
-            /*
-            if (!validateItems(itemsFromCurrentPage)) {
-                logger.info("validate items");
-                resetAuc();
-                auctionManager = wowInstance.getAuctionManager();
-                continue;
-            }
-            */
+
             for (int i = 0; i < itemsFromCurrentPage.length; i++) {
                 Item item = itemsFromCurrentPage[i];
 
                 BuyingItem buyingItem = analyzer.buyItem(item, i + 1);
 
                 if (buyingItem.getBuyType() == BuyType.BUYOUT) {
-                    int haveChangesIterNumber = -1;
-                    for (int iteration = 0; iteration < 5; iteration++) {
-                        Item[] secondScan = auctionManager.getItemsFromCurrentPage();
-                        if (!item.compareFields(secondScan[i])) {
-                            logger.info("items are not equals first:{} second:{}", item, secondScan[i]);
-                        }
-                        Utils.sleep(1);
-                    }
-                    if (haveChangesIterNumber == -1) {
-                        if (!TEST_FLAG) {
-                            analyzer.buyItem(item, buyingItem.getBuyType(), buyingItem.getIndex(), 1, false);
-                        }
-                        Utils.sleep(3000);
+                    Item[] secondScan = auctionManager.getItemsFromCurrentPage();
+                    logger.info("found item for at index:{} , item:{} ", i, item);
+                    if (item.compareFields(secondScan[i])) {
+                        analyzer.buyItem(item, buyingItem.getBuyType(), buyingItem.getIndex(), 1, false);
+                        // make screenshot of errors or success buying
+                        wowInstance.click(WinKey.P);
                     } else {
-                        logger.info("found page which are not equals to previous on iteration:" + haveChangesIterNumber);
+                        logger.info("items are not equals, item:{} , second item:{}", item, secondScan[i]);
                     }
+                    analyzer.buyItem(item, buyingItem.getBuyType(), buyingItem.getIndex(), 1, false);
                 }
             }
             wowInstance.click(WinKey.D5);
-            if (!TEST_FLAG) {
-                //Utils.sleep(50);
-            }
         }
+
     }
 
     void resetOnFirstPage() {
@@ -251,12 +308,6 @@ public class Buyer {
                 Utils.sleep(100L);
                 wowInstance.click(WinKey.W);
                 Utils.sleep(100);
-                wowInstance.click(WinKey.W);
-                Utils.sleep(100);
-                wowInstance.click(WinKey.S, 52);
-                Utils.sleep(100);
-                wowInstance.click(WinKey.S, 52);
-                Utils.sleep(100);
                 wowInstance.click(WinKey.S, 52);
                 Utils.sleep(1000L);
                 wowInstance.click(WinKey.D2);
@@ -266,7 +317,8 @@ public class Buyer {
             Utils.sleep(1000);
             wowInstance.click(WinKey.D5);
             Utils.sleep(2000);
-        } catch (Throwable ignore) {}
+        } catch (Throwable ignore) {
+        }
     }
 
     private void resetTmpFile() throws IOException {
