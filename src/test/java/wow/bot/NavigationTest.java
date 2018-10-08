80a3422fdddfabd7a9d93350c0ef7bc9d740e78e
alexlovkov <charmik1994@gmail.com>
2018-10-08 04:06:56 +0300

    remove Navigation.Coordinates3D class

--- src/test/java/wow/bot/NavigationTest.java
+++ src/test/java/wow/bot/NavigationTest.java
@@ -2,22 +2,23 @@ package wow.bot;
 
 import java.util.Optional;
 
-import javafx.geometry.Coordinates;
 import org.junit.Ignore;
 import org.junit.Test;
 import util.Utils;
 import wow.BaseTest;
-import wow.components.Navigation;
+import wow.components.Coordinates;
 import wow.memory.objects.CreatureObject;
 
 import static wow.components.Navigation.areNear;
 import static wow.components.Navigation.areNearAsCaster;
-import static wow.components.Navigation.get2DCoordsFor;
+import static wow.components.Navigation.evaluateDistanceFromTo;
+import static wow.components.Navigation.get3DCoordsFor;
 
 /**
  * @author Cargeh
  */
 public class NavigationTest extends BaseTest {
+
     @Ignore
     @Test
     public void testPlayerNearTarget() {
@@ -35,9 +36,9 @@ public class NavigationTest extends BaseTest {
     public void testDistance() {
         Optional<CreatureObject> target = player.getTarget();
         target.ifPresent(ctmManager::moveTo);
-        Navigation.Coordinates3D initial = get2DCoordsFor(player);
+        Coordinates initial = get3DCoordsFor(player);
         Utils.sleep(500);
-        Navigation.Coordinates3D current = get2DCoordsFor(player);
+        Coordinates current = get3DCoordsFor(player);
         double v = evaluateDistanceFromTo(initial, current);
         System.out.println(v);
         ctmManager.stop();
@@ -48,7 +49,7 @@ public class NavigationTest extends BaseTest {
     public void testDistanceCast() {
         Optional<CreatureObject> target = player.getTarget();
         for (; ; ) {
-            Navigation.Coordinates3D initial = get2DCoordsFor(player);
+            Coordinates initial = get3DCoordsFor(player);
             target.ifPresent(creatureObject -> areNearAsCaster(initial, creatureObject));
             Utils.sleep(100);
         }
@@ -66,11 +67,11 @@ public class NavigationTest extends BaseTest {
     @Ignore
     @Test
     public void goToPoint() {
-        Coordinates p1 = new Coordinates(2523.686, 1596.5973, 1269.3445);
-        Coordinates p2 = new Coordinates(2445.8625, 1596.8517, 1199.2115);
+        Coordinates p1 = new Coordinates(2523.686f, 1596.5973f, 1269.3445f);
+        Coordinates p2 = new Coordinates(2445.8625f, 1596.8517f, 1199.2115f);
         System.out.println(p1.distance(p2));
         for (; ; ) {
-            ctmManager.moveTo(new Coordinates(2046.6353, 1647.0458, 1170.6542));
+            ctmManager.moveTo(new Coordinates(2046.6353f, 1647.0458f, 1170.6542f));
         }
     }
 }
