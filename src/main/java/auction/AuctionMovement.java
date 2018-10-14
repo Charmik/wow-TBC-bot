5d109148dcbbed6e6314808e05c565cd2e936b46
alexlovkov <charmik1994@gmail.com>
2018-10-14 11:50:17 +0300

    add auction in Iron, alliance

--- src/main/java/auction/AuctionMovement.java
+++ src/main/java/auction/AuctionMovement.java
@@ -17,7 +17,10 @@ public class AuctionMovement {
 
     private static final Coordinates STORMWIND_MAIL_BOX = new Coordinates(-8876.547f, 651.18896f, 96.040054f);
     private static final Coordinates STORMWIND_AUCTION = new Coordinates(-8814.421f, 661.2754f, 95.42299f);
-    // TODO: real coordinates
+
+    private static final Coordinates IRON_MAIL_BOX = new Coordinates(-4911.0693f, -975.3947f, 501.4486f);
+    private static final Coordinates IRON_AUCTION = new Coordinates(-4962.4834f, -905.2312f, 503.8378f);
+
     private static final Coordinates THUNDER_BLUFF_MAILBOX = new Coordinates(-1263.1705f, 45.47196f, 127.60867f);
     private static final Coordinates THUNDER_BLUFF_AUCTION = new Coordinates(-1210.8663f, 95.01946f, 134.33122f);
 
@@ -27,16 +30,16 @@ public class AuctionMovement {
     private final Coordinates mailboxCoordinates;
     private final Coordinates auctionCoordinates;
 
-    public AuctionMovement(WowInstance wowInstance) {
+    AuctionMovement(WowInstance wowInstance) {
         this.movement = new Movement(wowInstance.getPlayer(), wowInstance.getCtmManager(), wowInstance, wowInstance.getObjectManager());
         this.wowInstance = wowInstance;
-        globalGraph.buildGlobalGraph();
+        this.globalGraph.buildGlobalGraph();
         if (wowInstance.getPlayer().getFaction().isAlliance()) {
-            mailboxCoordinates = STORMWIND_MAIL_BOX;
-            auctionCoordinates = STORMWIND_AUCTION;
+            this.mailboxCoordinates = IRON_MAIL_BOX;
+            this.auctionCoordinates = IRON_AUCTION;
         } else {
-            mailboxCoordinates = THUNDER_BLUFF_MAILBOX;
-            auctionCoordinates = THUNDER_BLUFF_AUCTION;
+            this.mailboxCoordinates = THUNDER_BLUFF_MAILBOX;
+            this.auctionCoordinates = THUNDER_BLUFF_AUCTION;
         }
     }
 
@@ -47,16 +50,19 @@ public class AuctionMovement {
         goTo(path);
     }
 
-    public void goToAuction() {
+    void goToAuction() {
         logger.info("goToAuction");
         List<Graph.Vertex> path = globalGraph.getShortestPathFromPlayerToPoint(
             wowInstance.getPlayer(), auctionCoordinates);
         goTo(path);
     }
 
-    public void goTo(List<Graph.Vertex> path) {
+    private void goTo(List<Graph.Vertex> path) {
         for (Graph.Vertex vertex : path) {
-            movement.goToNextPoint(vertex.coordinates);
+            if (!movement.goToNextPoint(vertex.coordinates)) {
+                logger.info("couldn't go to the point:{}", vertex.coordinates);
+                return;
+            }
         }
         Utils.sleep(2000);
         Coordinates lastPoint = path.get(path.size() - 1).getCoordinates();
@@ -66,8 +72,21 @@ public class AuctionMovement {
         Utils.sleep(1000);
     }
 
-    public boolean farAwayFromAuction() {
+    boolean farAwayFromAuction() {
         double distance = auctionCoordinates.distance(wowInstance.getPlayer().getCoordinates());
-        return distance > 5;
+        boolean tooFarAway = distance > 5;
+        if (tooFarAway) {
+            logger.info("player is too far away from auction, distance: {} coordinates:{}",
+                distance, wowInstance.getPlayer().getCoordinates());
+        }
+        return tooFarAway;
+    }
+
+    public Coordinates getMailboxCoordinates() {
+        return mailboxCoordinates;
+    }
+
+    public Coordinates getAuctionCoordinates() {
+        return auctionCoordinates;
     }
 }
